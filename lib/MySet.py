17850de64e8426e1e40ca0d9f9594bfcb238cbec
Amene <layla_1380@yahoo.com>
2024-09-25 16:19:50 -0500

    craeting functions

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,24 @@
 class MySet:
+    def __init__(self, enumerable = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
 
-    pass
+    def has(self, value):
+        if value in self.dictionary:
+            return True
+        return False
+    def add(self, value):
+        if value not in self.dictionary:
+            self.dictionary[value] = True
+            return self.dictionary
+        
+    def delete(self, value):
+        if value in self.dictionary:
+            del self.dictionary[value]
+        return self.dictionary
+    def size(self):
+        return len(self.dictionary)
+    def clear(self):
+        self.dictionary.clear()
+
